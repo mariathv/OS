b4cd676762204c6882e653e8260944dd14ff5917
Maria Naeem Khokhar <114730306+mariathv@users.noreply.github.com>
2024-03-31 13:59:58 +0500

    Update re_main.c

--- A2/BoardGame_Q1/re_main.c
+++ A2/BoardGame_Q1/re_main.c
@@ -7,6 +7,7 @@
 #include <stdbool.h>
 #include<string.h>
 #include <termios.h>
+#include <signal.h>
 
 
 #include "structQ1.h"
@@ -20,12 +21,12 @@ void reset() {
     printf("\033[0m");
 }
 
-bool checkpos(int itemno, int x, int y, int itemsx[], int itemsy[], int totalp, int xcords[], int ycords[]){
+bool checkpos(int itemno, int itemsn, int x, int y, int itemsx[], int itemsy[], int totalp, int xcords[], int ycords[]){
    for(int i=0; i<totalp; i++){
         if(x==xcords[i] && y==ycords[i])
         return false;
    }
-   for(int i=0; i<3; i++){
+   for(int i=0; i<itemsn; i++){
     if (i==itemno)
     continue;
     if(x==itemsx[i] && y==itemsy[i])
@@ -35,43 +36,67 @@ bool checkpos(int itemno, int x, int y, int itemsx[], int itemsy[], int totalp, 
    return true;
 }
 
-void generateBoard(int n, int totalplayers, int xcords[] , int ycords[]) {
+bool isplayercollide(int totalp, int xcords[], int ycords[], int curr){
+    for(int i=0; i<totalp; i++){
+        if(i==curr) continue;
+        if(xcords[curr]==xcords[i] && ycords[curr]==ycords[i]){
+            printf("collision\n");int sl;
+            scanf("%d",&sl);
+        return true;
+        }
+    }
+    return false;
+}
 
-    //for(int i=0; i<totalplayers; i++){
-    //    printf("player %d xcord:%d  ycord:%d\n",i+1,xcords[i], ycords[i]);
-    //}
+void generateBoard(int n, int itemsn, int turnof, int totalplayers, int xcords[] , int ycords[], int xitems[], int yitems[]) {
+    printf("\033[1;32m");
     int i, j;
         int c=4; int k=0; // k -> total players
     for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
+        printf("\033[1;32m");
+        if(i==0)
+            printf("\033[1;97m");
+
+        for (j = 0; j < n+1; j++) {
+            if(i==0)
+            printf("====");
+            else
             printf("----");
         }
-        printf("-\n");
+        printf("\n");
         for (j = 0; j < n; j++) {
+            printf("\033[1;32m");
              bool flag=false;
             for(int k=0; k<totalplayers; k++){
                 if(i==ycords[k] && j==xcords[k]){
-                    k++;
+                    //k++;
                     flag= true;
-                    printf("|\033[1;9%dmXXX", c); reset();
-                    c++;
-                    if(c==6) c++;
-                    if(c==7) c=2;
+                    if(k==turnof){
+                        printf("|\033[1;107m   "); reset();
+                    }else
+                    printf("|\033[1;10%dm   ", c); reset();
+                    if(c==4) c=3;
+                    else if(c==3) c=5;
+                    else if(c==5) c=6;
                 }
             }
-            for(int k=0; k<3; k++){
-                
+            for(int k=0; k<itemsn; k++){
+                if(i==yitems[k] && j==xitems[k]){
+                    flag=true;
+                    printf("|\033[1;91m O "); reset();
+                }
             }
             if(!flag)
                 printf("|   ");
-
         }
         printf("|\n");
     }
-    for (j = 0; j < n; j++) {
-        printf("----");
+    printf("\033[1;97m");
+    for (j = 0; j < n+1; j++) {
+        printf("====");
     }
-    printf("-\n");
+    printf("\n");
+    reset();
 }
 
 
@@ -79,10 +104,10 @@ void generateBoard(int n, int totalplayers, int xcords[] , int ycords[]) {
 int main(){
     //just a setup for reading key without pressing enter (NON BLOCKING READ)
     struct termios oldt, newt;
-    tcgetattr(STDIN_FILENO, &oldt); // Get current terminal settings
+    tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
-    newt.c_lflag &= ~ICANON; // Disable canonical mode (line buffering)
-    newt.c_cc[VMIN] = 1;     // Set minimum number of characters for read to return (1)
+    newt.c_lflag &= ~ICANON; 
+    newt.c_cc[VMIN] = 1;    
     tcsetattr(STDIN_FILENO, TCSANOW, &newt); 
 
 
@@ -92,28 +117,27 @@ int main(){
     int gridnum = (812) / randnum;
     gridnum=gridnum%25;
 
-    // if(gridnum<10)
-    // gridnum+=15;
-    // if(gridnum>20)
-    // gridnum-=5;
+    if(gridnum<10)
+    gridnum+=15;
+    if(gridnum>18)
+    gridnum-=5;
     
     int numofplayers=0;
-    gridnum = 7;
+ 
 
     printf("grid size :: %d\n", gridnum);
     printf("enter number of players (max 5) ::");
     scanf("%d", &numofplayers);
-    //clear_input_buffer();
 
     int xCords[numofplayers];
     int yCords[numofplayers];
+
     //assign xcords
     xCords[0]=1; xCords[1]=gridnum-2;
     if(numofplayers>=3)
         xCords[2]=1;
     if(numofplayers>=4)
         xCords[3]=gridnum-2;
-
     //assign ycords
     yCords[0]=1; yCords[1]=gridnum-2;
     if(numofplayers>=3){
@@ -126,6 +150,7 @@ int main(){
     int maintoplayers[2];
     int playerstomain[2];
     int playertoplayer[2];
+    pid_t pids[4];
 
     if(pipe(playertoplayer)==-1){
         perror("pipe::"); exit(EXIT_FAILURE);
@@ -141,19 +166,18 @@ int main(){
         if(pid == -1){
             perror("fork::"); exit(EXIT_FAILURE);
         }
+        if(pid>0)
+            pids[i]=pid;
         if(pid == 0){
             pipemsg playeresend; pipemsg playerget;
             close(playerstomain[0]);
             close(maintoplayers[1]);
             int turnof=1;
             while(true){
-                //printf("child %d :: turn of %d\n", i+1, turnof);
                 if(turnof == i+1){
-                //printf("child %d reading\n", i+1);
+    
                 read(maintoplayers[0], &playerget, sizeof(playerget));
-               // printf("child %d read TURN OF %d, isMOVEDONE %d\n", i+1,playerget.turnof, playerget.isMoveDone);
                 if(playerget.isMoveDone==false && playerget.turnof==i+1){
-                    //printf("> PLAYER %d MOVING\n", i+1);
                     char keypressed;
                     bool correctkey=false;
                     do{
@@ -176,11 +200,15 @@ int main(){
                         correctkey=true;
                             break;
                         case 'd':
-                        if(xCords[i]==gridnum-1);
+                        if(xCords[i]==gridnum-1) break;
                         xCords[i]++;
                         correctkey=true;
                             break;
                         }
+                            
+                        }
+                        if(isplayercollide(numofplayers,xCords,yCords,i)){
+                                correctkey=false;
                         }
                     }while(!correctkey);
 
@@ -192,41 +220,32 @@ int main(){
                     write(playerstomain[1], &playeresend, sizeof(playeresend));
                     for(int i=0; i<numofplayers-1; i++)
                     write(playertoplayer[1], "done", 5);
-                    //printf("done sending xpos %d and ypos %d\n", playeresend.xPos, playeresend.yPos);
-                    //turnof++;
                     turnof++;
                     if(turnof>numofplayers) turnof=1;
                     sleep(1);
-                    //printf("child %d WROTE, turnof %d, ismovedone %d\n", i+1, playeresend.turnof, playeresend.isMoveDone);
                 }
                 }else{
                     char buffer[5];
-                    //printf("child %d waiting for another child\n",i+1);
                     read(playertoplayer[0], &buffer, 5);
                     turnof++;
                     if(turnof>numofplayers) turnof=1;
-                    //printf("child %d done waiting for another child\n", i+1);
                 }
-                //printf("child %d",i+1);
             }
         }
     }
     //MAIN PROCESS
 
-    //at a time 3 items will be generated
-    int itemxPos[3]={-1,-1,-1};
-    int itemyPos[3]={-1,-1,-1};
-    bool alrexsflag=false; //flag to check if item is generated a right place
-    for(int i=0; i<3; i++){
-        do{
-        alrexsflag=false;
-        itemxPos[i]=rand()%(gridnum-1);
-        itemyPos[i]=rand()%(gridnum-1);
-        if(checkpos(i, itemxPos[i], itemyPos[i],itemxPos, itemyPos,numofplayers,xCords, yCords)==false){
-            alrexsflag=true;
-        }
-        }while(alrexsflag);
+    //at a time (numofplayers+1) items will be generated
+    int itemsn=numofplayers+1;
+    int itemxPos[itemsn];
+    int itemyPos[itemsn];
+    for(int i=0; i<itemsn; i++){
+        itemxPos[i]=-1; itemyPos[i]=-1;
     }
+    bool alrexsflag=false; //flag to check if item is generated a right place
+    int score[numofplayers];
+    for(int i=0; i<numofplayers; i++)
+    score[i]=0;
 
 
     pipemsg mainsend; pipemsg mainget;
@@ -234,34 +253,57 @@ int main(){
     close(maintoplayers[0]);
     close(playerstomain[1]);
     int turnof = 0; bool firstmove = true;
+    int itemscollected=-1;
     while(true){
         //printf("reach\n");
         if(firstmove || mainget.turnof==turnof && mainget.isMoveDone == true){
             mainget.isMoveDone=false;
             if(!firstmove){
-            xCords[mainget.turnof-1]=mainget.xPos;
-            yCords[mainget.turnof-1]=mainget.yPos;
+                xCords[mainget.turnof-1]=mainget.xPos;
+                yCords[mainget.turnof-1]=mainget.yPos;
+                for(int i=0; i<itemsn; i++){
+                    if(mainget.xPos == itemxPos[i] && mainget.yPos == itemyPos[i]){
+                        itemxPos[i]=-1; itemyPos[i]=-1;
+                        itemscollected++;
+                        score[mainget.turnof-1]++;
+                        if(score[mainget.turnof-1]==3){
+                            printf("\033[1;94mPLAYER %d \033[1;92mWON!\n", mainget.turnof);
+                            for(int i=0; i<numofplayers;i++){
+                                kill(pids[i], SIGKILL);
+                            }
+                            exit(EXIT_SUCCESS);
+                        }
+                        break;
+                    }
+                }
+            }
+            if(itemscollected==-1 || itemscollected==itemsn){
+                itemscollected=0;
+                for(int i=0; i<itemsn; i++){
+                    do{
+                        alrexsflag=false;
+                        itemxPos[i]=rand()%(gridnum-1);
+                        itemyPos[i]=rand()%(gridnum-1);
+                        if(checkpos(i, itemsn, itemxPos[i], itemyPos[i],itemxPos, itemyPos,numofplayers,xCords, yCords)==false){
+                            alrexsflag=true;
+                        }
+                    }while(alrexsflag);
+                }
             }
             turnof++;
             if(turnof>numofplayers) turnof=1;
-            system("clear");
-            printf("PLAYER %d TURN::\n", turnof);
+            system("clear"); printf("|");
+            for(int i=0; i<numofplayers; i++){
+                printf("\033[1;92m  Player %d : \033[1;35m%d \033[1;92m|", i+1, score[i]);reset();
+            }
+            printf("\n---\n\033[1;94mPLAYER \033[1;95m%d \033[1;94mTURN::\n\n", turnof);reset();
             firstmove= false;
             mainsend.turnof=turnof;
-            generateBoard(gridnum,numofplayers,xCords,yCords);
-            //for(int i=0; i<numofplayers;i++)
+            generateBoard(gridnum, itemsn, mainsend.turnof-1, numofplayers,xCords,yCords, itemxPos, itemyPos);
             write(maintoplayers[1], &mainsend, sizeof(mainsend));
-            //printf("main wrote to child turnof :: %d, ismovedone:: %d\n", mainsend.turnof, mainsend.isMoveDone);
         }
-       // printf("main waiting for child %d to write\n", mainsend.turnof);
         read(playerstomain[0], &mainget, sizeof(pipemsg));
-        //printf("parent");
-        //sleep(1);
     }
 
 
 }
-
-//DEBUGGING TIPS
-//FOR LOOP FOR READING AND WRITING TO ALL CHILD?
-//DOUBLE CHECK PASSING OF X AND Y LOOP
